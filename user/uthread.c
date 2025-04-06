8452b0fd2ce43e9118db74ea8b8a6f286d79e063
VMaxxxxxxx <19855899874@163.com>
2025-04-06 15:50:48 +0800

    完成了uthread—switch上下文切换

--- user/uthread.c
+++ user/uthread.c
@@ -11,15 +11,41 @@
 #define MAX_THREAD  4
 
 
+// 线程切换需要保存的寄存器
+struct context
+{
+  uint64 ra;
+  uint64 sp;
+
+  // 由 被调用者 负责保存和恢复的寄存器
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;       // 在线程结构体中，添加context结构体
 
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+// extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context* old, struct context* new);
+
               
+
 void 
 thread_init(void)
 {
@@ -63,6 +89,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    // 实施两个线程的寄存器上下文切换
+    thread_switch(&t->context, &next_thread->context);
   } else
     next_thread = 0;
 }
@@ -72,11 +100,17 @@ thread_create(void (*func)())
 {
   struct thread *t;
 
+  // 找一个空闲线程槽
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+
+  // 返回地址，thread_switch线程切换执行完成后，返回到ra，设置成线程函数func，就可以切换后执行func
+  t->context.ra = (uint64)func;
+  // 帧指针，将线程的栈指针指向其独立的栈，栈的生长是从高地址到低地址，所以要将sp设置为指向stack的最高地址
+  t->context.sp = (uint64)&t->stack + (STACK_SIZE - 1);
 }
 
 void 
